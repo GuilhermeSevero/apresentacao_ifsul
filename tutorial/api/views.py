633e4c89ebc410e91e3d3aaaa931ed420a942e65
Guilherme Severo <guilhermehmsevero@hotmail.com>
2019-10-01 22:15:33 -0300

    Implementado ViewSet

--- tutorial/api/views.py
+++ tutorial/api/views.py
@@ -1,47 +1,8 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets
 
-from .serializers import UsuarioSerilizer, Usuario
+from .serializers import UsuarioSerializer, Usuario
 
 
-class UsuarioList(APIView):
-    def get(self, request, format=None):
-        usuario = Usuario.objects.all()
-        serializer = UsuarioSerilizer(usuario, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = UsuarioSerilizer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class UsuarioDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Usuario.objects.get(pk=pk)
-        except Usuario.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        usuario = self.get_object(pk)
-        serializer = UsuarioSerilizer(usuario)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        usuario = self.get_object(pk)
-        serializer = UsuarioSerilizer(usuario, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        usuario = self.get_object(pk)
-        usuario.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class UsuarioView(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
