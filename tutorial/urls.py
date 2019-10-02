b16326501a6a822ba7e219b2c228140b67481345
Guilherme Severo <guilhermehmsevero@hotmail.com>
2019-10-01 22:13:45 -0300

    Rotas no Braço 02

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+
 from tutorial.api import views
 
 urlpatterns = [
-    path('usuarios/', views.usuario_list),
-    path('usuarios/<int:pk>', views.usuario_detail),
+    path('usuarios/', views.UsuarioList.as_view()),
+    path('usuarios/<int:pk>', views.UsuarioDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
