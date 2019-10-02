c889f1f1a81b0280d826292316349ba1173ecdc4
Guilherme Severo <guilhermehmsevero@hotmail.com>
2019-10-01 22:05:10 -0300

    Adicionado model de Usuário

--- tutorial/api/models.py
+++ tutorial/api/models.py
@@ -1,3 +1,27 @@
+import datetime
+import math
 from django.db import models
 
+
 # Create your models here.
+
+
+class Usuario(models.Model):
+    class Meta:
+        db_table = 'usuario'
+        verbose_name = 'Usuários'
+        verbose_name_plural = 'usuarios'
+        ordering = ('id',)
+
+    nome = models.CharField(db_column='nome', max_length=255, null=False, blank=False)
+    cpf = models.CharField(db_column='cpf', max_length=11, null=True, blank=True, default=None)
+    data_nascimento = models.DateField(db_column='data_nascimento', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=False, blank=True)
+
+    @property
+    def idade(self):
+        if self.data_nascimento:
+            delta = datetime.date.today() - self.data_nascimento
+            return math.trunc(delta.days / 365)
+        return None
