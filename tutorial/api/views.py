0889694a9e79171627982a49fbe9b8dc36cb913f
Guilherme Severo <guilhermehmsevero@hotmail.com>
2019-10-01 22:18:25 -0300

    Adicionado Filtros e Ordenação

--- tutorial/api/views.py
+++ tutorial/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import UsuarioSerializer, Usuario
 
@@ -6,3 +8,7 @@ from .serializers import UsuarioSerializer, Usuario
 class UsuarioView(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['nome', 'cpf', 'data_nascimento']
+    ordering_fields = ['id', 'nome', 'cpf', 'data_nascimento', 'created_at', 'updated_at']
+    ordering = ['nome', 'id']
