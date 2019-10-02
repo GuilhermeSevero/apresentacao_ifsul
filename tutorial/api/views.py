1a9d51d1bddc752c498871f7bf4acfca49aecf95
Guilherme Severo <guilhermehmsevero@hotmail.com>
2019-10-01 23:12:18 -0300

    Adicionado ViewSet de Partidas

--- tutorial/api/views.py
+++ tutorial/api/views.py
@@ -1,8 +1,12 @@
-from rest_framework import viewsets
+from django.db.models import Count, Max
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 
-from .serializers import UsuarioSerializer, Usuario
+from .serializers import UsuarioSerializer, PartidaSerializer
+from .models import Usuario, Partida
 
 
 class UsuarioView(viewsets.ModelViewSet):
@@ -12,3 +16,22 @@ class UsuarioView(viewsets.ModelViewSet):
     filterset_fields = ['nome', 'cpf', 'data_nascimento']
     ordering_fields = ['id', 'nome', 'cpf', 'data_nascimento', 'created_at', 'updated_at']
     ordering = ['nome', 'id']
+
+    @action(methods=['GET'], detail=True)
+    def historico(self, request, pk, *args, **kwargs):
+        serializer = self.get_serializer()
+        return Response(data=serializer.get_historico(pk=pk), status=status.HTTP_200_OK)
+
+
+class PartidaView(viewsets.ModelViewSet):
+    queryset = Partida.objects.all()
+    serializer_class = PartidaSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = ['-created_at']
+
+    @action(methods=['GET'], detail=False)
+    def ranking(self, request, *args, **kwargs):
+        serializer = self.get_serializer()
+        return Response(data=serializer.get_ranking(), status=status.HTTP_200_OK)
